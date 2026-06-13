c83bdf85fd42e1332d81138d7d602730e0240ada
Ahmed Maresh <mareshahmed06@gmail.com>
2026-06-13 12:28:55 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/edit_note_view_body.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
@@ -7,44 +8,52 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 7),
-      child: Container(
-        padding: EdgeInsets.only(top: 20, bottom: 20, left: 16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Color(0xffFFCC80),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                'Flutter Tips',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => EditNoteViewBody()),
+          );
+        },
+        child: Container(
+          padding: EdgeInsets.only(top: 20, bottom: 20, left: 16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Color(0xffFFCC80),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  'Flutter Tips',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    'Build your career with flutter dart',
+                    style: TextStyle(color: Colors.black45, fontSize: 18),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.delete, color: Colors.black, size: 30),
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 10),
+              Padding(
+                padding: const EdgeInsets.only(right: 24, top: 14),
                 child: Text(
-                  'Build your career with flutter dart',
-                  style: TextStyle(color: Colors.black45, fontSize: 18),
+                  'May 21 , 2022',
+                  style: TextStyle(color: Colors.black45, fontSize: 12),
                 ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.delete, color: Colors.black, size: 30),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24, top: 14),
-              child: Text(
-                'May 21 , 2022',
-                style: TextStyle(color: Colors.black45, fontSize: 12),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
